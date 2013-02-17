d11c5eeed85c7fd2aed1d9c8911d3b1482144ccc
Ronald Kwan <rkwan@berkeley.edu>
2013-02-16 23:58:58 -0800

    sentiment scores successfully stored

--- fbstatuses.js
+++ fbstatuses.js
@@ -1,32 +1,41 @@
+var master;
+
 function getStatuses() {
-    FB.api('/me/statuses', { limit: 1 }, function(response) {
+    master = [];
+    FB.api('/me/statuses', { limit: 2 }, function(response) {
 	var statuses = response['data'];
+	var cleaned = {};
 	for (var i = 0; i < statuses.length; i++) {
 	    var status = statuses[i]
-	    var id = status['id'];
-	    var time = status['updated_time'];
-	    var msg = status['message'];
-	    var sentiment = getSentiment(msg);
-	    $('#statusTable').find('tbody')
-		.append($('<tr>')
-			.append($('<td>')
-				.text(time))
-			.append($('<td>')
-				.text(msg))
-			.append($('<td>')
-				.text(sentiment)));
+	    var clean = { time: status['updated_time'],
+			  msg: status['message'] }
+	    setSentiment(clean);
 	}
     });
 }
 
-function getSentiment(msg) {
+function setSentiment(status) {
     var key = "";
     $.get('key.txt', function(data) {
 	key = data.replace(/\s/gm, '');
-	console.log(key);
 	var lymbix = $.lymbix(key);
+	var msg = status['msg'];
 	lymbix.tonalize(msg, function(data) {
-	    return data['article_sentiment']['score'];
+	    var score = data['article_sentiment']['score'];
+	    status['score'] = score;
+	    writeToTable(status);
+	    master.push(status)
 	});
     });
+}
+
+function writeToTable(status) {
+    $('#statusTable').find('tbody')
+	.append($('<tr>')
+		.append($('<td>')
+			.text(status['time']))
+		.append($('<td>')
+			.text(status['msg']))
+		.append($('<td>')
+			.text(status['score'])));
 }
